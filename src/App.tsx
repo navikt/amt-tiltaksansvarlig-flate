6f99677913deb1a2b8ecc7a1806d8349765f1ce6
Alexander Gård <alex.gaard@hotmail.com>
2021-10-08 12:02:15 +0200

    Flyttet authentication sjekk ut til App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { AxiosResponse } from 'axios'
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import { IsAuthenticated,isAuthenticated } from './api/api'
+import { LoginPage } from './component/page/login/LoginPage'
 import { MainPage } from './component/page/main/MainPage'
+import { isNotStartedOrPending, isRejected, usePromise } from './utils/use-promise'
 
 export const App = (): React.ReactElement => {
-	// if (isNotStartedOrPending(fetchInnloggetAnsattPromise)) {
-	// 	return '...'
-	// }
-	//
-	// if (isRejected(fetchInnloggetAnsattPromise)) {
-	// 	return <LoginPage />
-	// }
+	const isAuthenticatedPromise = usePromise<AxiosResponse<IsAuthenticated>>(isAuthenticated)
+
+	if (isNotStartedOrPending(isAuthenticatedPromise)) {
+		return <div>...</div>
+	}
+
+	if (isRejected(isAuthenticatedPromise) || !isAuthenticatedPromise?.result.data.isAuthenticated) {
+		return <LoginPage />
+	}
 
 	return (
 		<BrowserRouter>
--- src/component/page/main/MainPage.tsx
+++ src/component/page/main/MainPage.tsx
@@ -1,21 +1,6 @@
-import { AxiosResponse } from 'axios'
 import React from 'react'
 
-import { IsAuthenticated,isAuthenticated } from '../../../api/api'
-import { isNotStartedOrPending, isRejected, usePromise } from '../../../utils/use-promise'
-import { LoginPage } from '../login/LoginPage'
-
 export const MainPage = (): React.ReactElement => {
-	const isAuthenticatedPromise = usePromise<AxiosResponse<IsAuthenticated>>(isAuthenticated)
-
-	if (isNotStartedOrPending(isAuthenticatedPromise)) {
-		return <div>...</div>
-	}
-
-	if (isRejected(isAuthenticatedPromise) || !isAuthenticatedPromise?.result.data.isAuthenticated) {
-		return <LoginPage />
-	}
-
 	return (
 		<main>
 			<h1>Du er logget inn</h1>
