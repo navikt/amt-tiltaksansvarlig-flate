bbc1866dc93da11e7e1edde8e2ea2c73a7a0fa00
Alexander Gård <alex.gaard@hotmail.com>
2022-03-29 15:08:44 +0200

    Bruk custom lenkepanel med Link fra react-router-dom
    Siden vi har en SPA-app så burde alle interne lenker bruke react-router-dom så vi slipper å laste inn hele siden på nytt

--- src/component/GjennomforingPanel.tsx
+++ src/component/GjennomforingPanel.tsx
@@ -1,9 +1,10 @@
-import { Heading, LinkPanel } from '@navikt/ds-react'
+import { Heading } from '@navikt/ds-react'
 import React from 'react'
 
 import styles from './GjennomforingPanel.module.scss'
 import { Gjennomforing } from '../api/api'
 import { appUrl } from '../utils/url-utils'
+import { SpaLenkepanel } from './spa-lenkepanel/SpaLenkepanel'
 
 interface GjennomforingPanelProps {
 	gjennomforing : Gjennomforing
@@ -11,10 +12,10 @@ interface GjennomforingPanelProps {
 
 export const GjennomforingPanel = ({ gjennomforing }: GjennomforingPanelProps) : React.ReactElement => {
 	return (
-		<LinkPanel href={appUrl(`/gjennomforing/${gjennomforing.id}`)} className={styles.panel}>
+		<SpaLenkepanel to={appUrl(`/gjennomforing/${gjennomforing.id}`)} className={styles.panel}>
 			<Heading size="xsmall">
 				{gjennomforing.navn}
 			</Heading>
-		</LinkPanel>
+		</SpaLenkepanel>
 	)
 }
