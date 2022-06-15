63c87cc092fe36b6a5348736c9b6f83dbe79ca79
Alexander Gård <alex.gaard@hotmail.com>
2022-06-15 10:17:02 +0200

    Flyttet knapp for å fjerne tiltak fra oversikten

---
+++ src/component/Show.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+interface ShowProps {
+	if?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+	children: React.ReactNode;
+}
+
+export const Show = (props: ShowProps): JSX.Element | null => (props.if ? <>{props.children}</> : null)
