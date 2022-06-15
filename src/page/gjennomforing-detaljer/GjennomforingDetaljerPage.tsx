43c0e313b7ad40cd856d91cf483dcb13bec7beef
Alexander Gård <alex.gaard@hotmail.com>
2022-06-15 10:17:02 +0200

    Flyttet knapp for å fjerne tiltak fra oversikten

--- src/page/gjennomforing-detaljer/GjennomforingDetaljerPage.tsx
+++ src/page/gjennomforing-detaljer/GjennomforingDetaljerPage.tsx
@@ -3,27 +3,35 @@ import React from 'react'
 
 import { GjennomforingGenerellInfo } from '../../component/GjennomforingGenerellInfo'
 import { useParams } from 'react-router-dom'
-import { isNotStartedOrPending, isPending, isRejected, isResolved, usePromise } from '../../utils/use-promise'
-import { fetchGjennomforing, stopTilgangTilGjennomforing, GjennomforingDetaljerType } from '../../api/api'
+import {
+	isFinished,
+	isNotStartedOrPending,
+	isPending,
+	isRejected,
+	isResolved,
+	usePromise
+} from '../../utils/use-promise'
+import { fetchGjennomforing, fjernGjennomforingFraOversikten, GjennomforingDetaljerType } from '../../api/api'
 import { AxiosResponse } from 'axios'
 import styles from './GjennomforingDetaljerPage.module.scss'
 import globalStyles from '../../globals.module.scss'
 import { Tilbakelenke } from '../../component/tilbakelenke/Tilbakelenke'
 import { FORSIDE_PAGE_ROUTE } from '../../navigation'
 import { Endringsmeldinger } from './endringsmeldinger/Endringsmeldinger'
+import { Show } from '../../component/Show'
 
 export const GjennomforingDetaljerPage = () : React.ReactElement => {
 	const { gjennomforingId } = useParams()
 
 	const gjennomforingPromise = usePromise<AxiosResponse<GjennomforingDetaljerType>>(() => fetchGjennomforing(gjennomforingId!))
-	const stopTilgangTilGjennomforingPromise = usePromise<AxiosResponse>()
+	const fjernFraOversiktenPromise = usePromise<AxiosResponse>()
 
 	if (isNotStartedOrPending(gjennomforingPromise)) return <Loader/>
 
 	if (isRejected(gjennomforingPromise)) return <Alert variant="error">En feil har oppstått</Alert>
 
 	const handleFjernFraMinOversikt = () => {
-		stopTilgangTilGjennomforingPromise.setPromise(() => stopTilgangTilGjennomforing(gjennomforingId!))
+		fjernFraOversiktenPromise.setPromise(() => fjernGjennomforingFraOversikten(gjennomforingId!))
 	}
 
 	const gjennomforing = gjennomforingPromise.result.data
@@ -32,40 +40,38 @@ export const GjennomforingDetaljerPage = () : React.ReactElement => {
 		<main className={styles.page}>
 			<Tilbakelenke to={FORSIDE_PAGE_ROUTE} className={globalStyles.blokkM}/>
 
-			<Heading size="medium" spacing>{gjennomforing.navn}</Heading>
+			<div className={styles.heading}>
+				<Heading size="medium" spacing>{gjennomforing.navn}</Heading>
+
+				<Show if={isNotStartedOrPending(fjernFraOversiktenPromise)}>
+					<Button
+						variant="secondary"
+						size="small"
+						onClick={handleFjernFraMinOversikt}
+						className={globalStyles.blokkXs}
+						loading={isPending(fjernFraOversiktenPromise)}
+						disabled={isPending(fjernFraOversiktenPromise) || isResolved(fjernFraOversiktenPromise)}
+					>
+						Fjern fra min oversikt
+					</Button>
+				</Show>
+
+				<Show if={isFinished(fjernFraOversiktenPromise)}>
+					{
+						isResolved(fjernFraOversiktenPromise) &&
+							(<Alert variant="success" size="small" className={styles.alert}>Tiltaket er fjernet fra oversikten</Alert>)
+					}
+
+					{
+						isRejected(fjernFraOversiktenPromise) &&
+							(<Alert variant="error" size="small" className={styles.alert}>Noe gikk galt</Alert>)
+					}
+				</Show>
+			</div>
 
 			<GjennomforingGenerellInfo gjennomforing={gjennomforing}/>
 
 			<Endringsmeldinger gjennomforingId={gjennomforingId!}/>
-
-			<div className={styles.seperator}/>
-
-			<section className={styles.fjernFraMinOversiktSection}>
-				<Heading size="small" level="2" className={globalStyles.blokkXs}>
-					Tiltaket er lagt til i oversikten
-				</Heading>
-
-				<Button
-					variant="secondary"
-					size="small"
-					onClick={handleFjernFraMinOversikt}
-					className={globalStyles.blokkXs}
-					loading={isPending(stopTilgangTilGjennomforingPromise)}
-					disabled={isPending(stopTilgangTilGjennomforingPromise) || isResolved(stopTilgangTilGjennomforingPromise)}
-				>
-					Fjern fra min oversikt
-				</Button>
-
-				{
-					isResolved(stopTilgangTilGjennomforingPromise) &&
-					(<Alert variant="success" className={styles.alert}>Tiltaket er fjernet fra oversikten</Alert>)
-				}
-
-				{
-					isRejected(stopTilgangTilGjennomforingPromise) &&
-					(<Alert variant="error" className={styles.alert}>Noe gikk galt</Alert>)
-				}
-			</section>
 		</main>
 	)
 }
