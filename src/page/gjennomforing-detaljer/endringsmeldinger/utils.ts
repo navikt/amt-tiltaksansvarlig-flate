92753b46e7af7e1e89d1e645a5845f6ea9e7e4bc
Izis Frøyhov <izis.froyhov@nav.no>
2022-11-23 16:01:02 +0100

    Sorterer endringsmeldinger på etternavn og opprettetdato (#97)

--- src/page/gjennomforing-detaljer/endringsmeldinger/utils.ts
+++ src/page/gjennomforing-detaljer/endringsmeldinger/utils.ts
@@ -1,5 +1,6 @@
-type Endringsmelding = { opprettetDato: Date }
+import { Endringsmelding } from '../../../api/schema/endringsmelding'
 
-export const sorterEndringsmeldingNyestFørst = (e1: Endringsmelding, e2: Endringsmelding): number => {
-	return e1.opprettetDato > e2.opprettetDato ? -1 : 1
+export const sorterEndringsmeldinger = (e1: Endringsmelding, e2: Endringsmelding): number => {
+	if(e1.deltaker.etternavn === e2.deltaker.etternavn) return e1.opprettetDato > e2.opprettetDato ? -1 : 1
+	return e1.deltaker.etternavn > e2.deltaker.etternavn? 1 : -1
 }
