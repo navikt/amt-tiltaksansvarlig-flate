553989e013878e92510e4cfcc72e7311727eb51b
Tormod Fossum <tormod.fossum@nav.no>
2022-11-25 13:17:18 +0100

    Oppdater informasjon om endringsmeldinger

--- src/page/gjennomforing-detaljer/endringsmeldinger/endringsmelding/EndringsmeldingPanel.tsx
+++ src/page/gjennomforing-detaljer/endringsmeldinger/endringsmelding/EndringsmeldingPanel.tsx
@@ -45,7 +45,7 @@ export const EndringsmeldingPanel = ({ endringsmelding, onFerdig, varighetValg, 
 			</div>
 			<div className={styles.meldingRow}>
 				<PanelLinje>
-					<Heading size="xsmall" level="4">{navn}</Heading>
+					<Heading size="xsmall" level="3">{navn}</Heading>
 					<BodyShort size="medium" className={styles.fnr} >{deltaker.fodselsnummer}</BodyShort>
 					<Detail size="small" className={styles.sendt}>Sendt: {formatDate(endringsmelding.opprettetDato)}</Detail>
 				</PanelLinje>
