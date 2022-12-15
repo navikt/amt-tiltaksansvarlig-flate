9f49baf02b5fd6cce94ecdd38fe1a1c88672beec
Alexander Gård <AlexGaard@users.noreply.github.com>
2022-12-15 09:07:11 +0100

    Omrokkering av gjennomføring detaljer og visning av orgnr (#108)

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -7,7 +7,9 @@ interface Gjennomforing {
     id: string,
     arrangor: {
         organisasjonNavn: string | null,
-        virksomhetNavn: string
+        organisasjonOrgnr: string | null,
+        virksomhetNavn: string,
+        virksomhetOrgnr: string,
     },
     lopenr: number,
     opprettetAr: number,
@@ -32,7 +34,9 @@ export const gjennomforinger: Gjennomforing[] = [
 		id: '6ec95b2a-be19-41f0-9c97-1f81ab2159c3',
 		arrangor: {
 			organisasjonNavn: 'Venus AS',
-			virksomhetNavn: 'Virksomhet AS'
+			organisasjonOrgnr: '9934872368',
+			virksomhetNavn: 'Virksomhet AS',
+			virksomhetOrgnr: '8798324354',
 		},
 		lopenr: 31243,
 		opprettetAr: 2022,
@@ -49,8 +53,9 @@ export const gjennomforinger: Gjennomforing[] = [
 		id: 'a7f1ac51-4a7a-4d9b-b239-db452a9ee2c7',
 		arrangor: {
 			organisasjonNavn: 'Merkur AS',
-			virksomhetNavn: 'Virksomhet AS'
-
+			organisasjonOrgnr: '409857349',
+			virksomhetNavn: 'Virksomhet AS',
+			virksomhetOrgnr: '56546745654'
 		},
 		lopenr: 9243,
 		opprettetAr: 2022,
@@ -67,7 +72,9 @@ export const gjennomforinger: Gjennomforing[] = [
 		id: 'c1a4ae05-983e-4f49-9e35-592a04248379',
 		arrangor: {
 			organisasjonNavn: null,
-			virksomhetNavn: 'Pluto AS'
+			organisasjonOrgnr: null,
+			virksomhetNavn: 'Pluto AS',
+			virksomhetOrgnr: '48930578349'
 		},
 		lopenr: 75634,
 		opprettetAr: 2022,
@@ -84,7 +91,9 @@ export const gjennomforinger: Gjennomforing[] = [
 		id: '70a660b0-6529-421f-b957-042a9499986e',
 		arrangor: {
 			organisasjonNavn: null,
-			virksomhetNavn: 'Mars AS'
+			organisasjonOrgnr: null,
+			virksomhetNavn: 'Mars AS',
+			virksomhetOrgnr: '34290472398'
 		},
 		lopenr: 67182,
 		opprettetAr: 2022,
