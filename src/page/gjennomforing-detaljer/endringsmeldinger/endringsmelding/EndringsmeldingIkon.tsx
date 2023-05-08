19852357d8530a0b704c732f3c96f1666c198533
Izis Frøyhov <izis.froyhov@nav.no>
2023-05-08 13:24:52 +0200

    Oppdaterer ikoner (#123)

--- src/page/gjennomforing-detaljer/endringsmeldinger/endringsmelding/EndringsmeldingIkon.tsx
+++ src/page/gjennomforing-detaljer/endringsmeldinger/endringsmelding/EndringsmeldingIkon.tsx
@@ -1,11 +1,11 @@
-import { CheckmarkCircleFillIcon, MenuElipsisHorizontalCircleFillIcon } from '@navikt/aksel-icons'
 import {
-	AddCircleFilled,
-	ChevronRightCircleFilled,
-	ChevronRightDoubleFilled,
-	MinusCircleFilled
-} from '@navikt/ds-icons'
-import SvgDivideFilled from '@navikt/ds-icons/esm/DivideFilled'
+	CheckmarkCircleFillIcon,
+	ChevronRightCircleFillIcon,
+	ChevronRightDoubleCircleFillIcon,
+	MenuElipsisHorizontalCircleFillIcon,
+	MinusCircleFillIcon,
+	PieChartFillIcon,
+	PlusCircleFillIcon } from '@navikt/aksel-icons'
 import React from 'react'
 
 import { EndringsmeldingType } from '../../../../api/schema/endringsmelding'
@@ -18,22 +18,20 @@ interface Props {
 export const EndringsmeldingIkon = ({ type }: Props): React.ReactElement => {
 	switch (type) {
 		case EndringsmeldingType.LEGG_TIL_OPPSTARTSDATO:
-			return <ChevronRightCircleFilled className={styles.endreIkon} aria-hidden/>
 		case EndringsmeldingType.ENDRE_OPPSTARTSDATO:
-			return <ChevronRightCircleFilled className={styles.endreIkon} aria-hidden/>
+			return <ChevronRightCircleFillIcon className={styles.endreIkon} aria-hidden/>
 		case  EndringsmeldingType.FORLENG_DELTAKELSE:
-			return <ChevronRightDoubleFilled className={styles.forlengIkon} aria-hidden/>
+			return <ChevronRightDoubleCircleFillIcon className={styles.forlengIkon} aria-hidden/>
 		case EndringsmeldingType.AVSLUTT_DELTAKELSE:
-			return <MinusCircleFilled className={styles.avsluttIkon} aria-hidden/>
+		case EndringsmeldingType.ENDRE_SLUTTDATO:
+			return <MinusCircleFillIcon className={styles.avsluttIkon} aria-hidden/>
 		case EndringsmeldingType.ENDRE_DELTAKELSE_PROSENT:
-			return <SvgDivideFilled className={styles.endreProsentDeltakelseIkon} aria-hidden/>
+			return <PieChartFillIcon className={styles.endreProsentDeltakelseIkon} aria-hidden/>
 		case EndringsmeldingType.DELTAKER_IKKE_AKTUELL:
-			return <AddCircleFilled className={styles.ikkeAktuellIkon} aria-hidden/>
+			return <PlusCircleFillIcon className={styles.ikkeAktuellIkon} aria-hidden/>
 		case EndringsmeldingType.TILBY_PLASS:
 			return <CheckmarkCircleFillIcon className={styles.tilbyPlassIkon} aria-hidden />
 		case EndringsmeldingType.SETT_PAA_VENTELISTE:
 			return <MenuElipsisHorizontalCircleFillIcon className={styles.settPaaVentelisteIkon} aria-hidden />
-		case EndringsmeldingType.ENDRE_SLUTTDATO:
-			return <MinusCircleFilled className={styles.avsluttIkon} aria-hidden/>
 	}
 }
