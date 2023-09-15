1c93e8fbaadc3c147457725813e96ebd47046246
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2023-09-15 11:57:26 +0200

    Legge til accordion rundt alle tiltakene så de kan utvides/lukkes. Husk state om åpen / lukket

--- src/page/forside/gjennomforing-liste/GjennomforingListe.tsx
+++ src/page/forside/gjennomforing-liste/GjennomforingListe.tsx
@@ -1,17 +1,20 @@
-import { Heading } from '@navikt/ds-react'
+import { Accordion } from '@navikt/ds-react'
 import React, { useEffect } from 'react'
 
 import { Gjennomforing, Tiltak } from '../../../api/api'
-import globalStyles from '../../../globals.module.scss'
 import { sortAlphabetic } from '../../../utils/sort-utils'
 import { GjennomforingPanel } from './gjennomforing-panel/GjennomforingPanel'
 import { TomVisningPanel } from './tom-visning-panel/TomVisningPanel'
+import styles from './GjennomforingListe.module.scss'
+import useLocalStorage from '../../../hooks/useLocalStorage'
 
 interface Props {
 	gjennomforinger: Gjennomforing[]
 }
 
 export const GjennomforingListe = (props: Props): React.ReactElement => {
+
+
 	const handleClick = () => {
 		sessionStorage.setItem('scrollPosition', JSON.stringify(window.scrollY))
 	}
@@ -29,11 +32,15 @@ export const GjennomforingListe = (props: Props): React.ReactElement => {
 			{props.gjennomforinger.length === 0 ? (
 				<TomVisningPanel />
 			) : (
-				finnTiltakMedGjennomforinger(props.gjennomforinger)
-					.sort((tg1, tg2) => sortAlphabetic(tg1.tiltak.navn, tg2.tiltak.navn))
-					.map((tg) => {
-						return <GjennomforingGruppering tiltak={tg.tiltak} gjennomforinger={tg.gjennomforinger} key={tg.tiltak.kode} onClick={handleClick} />
-					})
+				<Accordion size="small">
+					{finnTiltakMedGjennomforinger(props.gjennomforinger)
+						.sort((tg1, tg2) => sortAlphabetic(tg1.tiltak.navn, tg2.tiltak.navn))
+						.map((tg) => {
+							return (
+								<GjennomforingGruppering tiltak={tg.tiltak} gjennomforinger={tg.gjennomforinger} key={tg.tiltak.kode} onClick={handleClick} />
+							)
+						})}
+				</Accordion>
 			)}
 		</section>
 	)
@@ -64,16 +71,18 @@ interface GjennomforingGrupperingProps {
 }
 
 const GjennomforingGruppering = (props: GjennomforingGrupperingProps): React.ReactElement => {
+	const [tiltakOpen, setTiltakOpen] = useLocalStorage(`accordion-open-${props.tiltak.navn}`, true)
+
 	return (
-		<div className={globalStyles.blokkM}>
-			<Heading size="xsmall" level="2">
-				{props.tiltak.navn}
-			</Heading>
-			{props.gjennomforinger
-				.sort((g1, g2) => sortAlphabetic(g1.navn, g2.navn))
-				.map((gjennomforing) => (
-					<GjennomforingPanel onClick={props.onClick} gjennomforing={gjennomforing} key={gjennomforing.id} />
-				))}
-		</div>
+		<Accordion.Item defaultOpen={tiltakOpen}>
+			<Accordion.Header onClick={() => setTiltakOpen(!tiltakOpen)}>{props.tiltak.navn}</Accordion.Header>
+			<Accordion.Content className={styles.accordionContent}>
+				{props.gjennomforinger
+					.sort((g1, g2) => sortAlphabetic(g1.navn, g2.navn))
+					.map((gjennomforing) => (
+						<GjennomforingPanel onClick={props.onClick} gjennomforing={gjennomforing} key={gjennomforing.id} />
+					))}
+			</Accordion.Content>
+		</Accordion.Item>
 	)
 }
