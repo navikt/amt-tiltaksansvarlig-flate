d50a1aaaa1d608f80d5397b5a17e4abd927f8348
MartheFH <136322563+MartheFH@users.noreply.github.com>
2023-09-20 11:07:39 +0200

    Legge scrollPossition i sessonStorage (#209)

--- src/page/forside/gjennomforing-liste/GjennomforingListe.tsx
+++ src/page/forside/gjennomforing-liste/GjennomforingListe.tsx
@@ -1,10 +1,11 @@
-import { Heading } from '@navikt/ds-react'
-import React from 'react'
+import { Accordion } from '@navikt/ds-react'
+import React, { useEffect } from 'react'
 
 import { Gjennomforing, Tiltak } from '../../../api/api'
-import globalStyles from '../../../globals.module.scss'
+import useLocalStorage from '../../../hooks/useLocalStorage'
 import { sortAlphabetic } from '../../../utils/sort-utils'
 import { GjennomforingPanel } from './gjennomforing-panel/GjennomforingPanel'
+import styles from './GjennomforingListe.module.scss'
 import { TomVisningPanel } from './tom-visning-panel/TomVisningPanel'
 
 interface Props {
@@ -12,20 +13,35 @@ interface Props {
 }
 
 export const GjennomforingListe = (props: Props): React.ReactElement => {
+
+
+	const handleClick = () => {
+		sessionStorage.setItem('scrollPosition', JSON.stringify(window.scrollY))
+	}
+
+	useEffect(() => {
+		const scrollPosition = sessionStorage.getItem('scrollPosition')
+		if (scrollPosition) {
+			window.scrollTo(0, JSON.parse(scrollPosition))
+			sessionStorage.removeItem('scrollPosition')
+		}
+	}, [])
+
 	return (
 		<section>
-			{props.gjennomforinger.length === 0
-				? (<TomVisningPanel />)
-				: (finnTiltakMedGjennomforinger(props.gjennomforinger)
-					.sort((tg1, tg2) => sortAlphabetic(tg1.tiltak.navn, tg2.tiltak.navn))
-					.map(tg => {
-						return <GjennomforingGruppering
-							tiltak={tg.tiltak}
-							gjennomforinger={tg.gjennomforinger}
-							key={tg.tiltak.kode}
-						/>
-					}))
-			}
+			{props.gjennomforinger.length === 0 ? (
+				<TomVisningPanel />
+			) : (
+				<Accordion size="small">
+					{finnTiltakMedGjennomforinger(props.gjennomforinger)
+						.sort((tg1, tg2) => sortAlphabetic(tg1.tiltak.navn, tg2.tiltak.navn))
+						.map((tg) => {
+							return (
+								<GjennomforingGruppering tiltak={tg.tiltak} gjennomforinger={tg.gjennomforinger} key={tg.tiltak.kode} onClick={handleClick} />
+							)
+						})}
+				</Accordion>
+			)}
 		</section>
 	)
 }
@@ -48,15 +64,25 @@ const finnTiltakMedGjennomforinger = (gjennomforinger: Gjennomforing[]): TiltakM
 }
 
 
-const GjennomforingGruppering = (props: TiltakMedGjennomforinger): React.ReactElement => {
+interface GjennomforingGrupperingProps {
+	tiltak: Tiltak
+	gjennomforinger: Gjennomforing[]
+	onClick: () => void
+}
+
+const GjennomforingGruppering = (props: GjennomforingGrupperingProps): React.ReactElement => {
+	const [ tiltakOpen, setTiltakOpen ] = useLocalStorage(`accordion-open-${props.tiltak.navn}`, true)
+
 	return (
-		<div className={globalStyles.blokkM}>
-			<Heading size="xsmall" level="2">{props.tiltak.navn}</Heading>
-			{
-				props.gjennomforinger
+		<Accordion.Item defaultOpen={tiltakOpen}>
+			<Accordion.Header onClick={() => setTiltakOpen(!tiltakOpen)}>{props.tiltak.navn}</Accordion.Header>
+			<Accordion.Content className={styles.accordionContent}>
+				{props.gjennomforinger
 					.sort((g1, g2) => sortAlphabetic(g1.navn, g2.navn))
-					.map(gjennomforing => <GjennomforingPanel gjennomforing={gjennomforing} key={gjennomforing.id} />)
-			}
-		</div>
+					.map((gjennomforing) => (
+						<GjennomforingPanel onClick={props.onClick} gjennomforing={gjennomforing} key={gjennomforing.id} />
+					))}
+			</Accordion.Content>
+		</Accordion.Item>
 	)
 }
