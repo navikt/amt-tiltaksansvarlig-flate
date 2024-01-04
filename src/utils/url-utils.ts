c6db2b9c8c2b6a35a02aab3cc476657f95d9d33f
Magnus Evensberget <magnus.evensberget@nav.no>
2024-01-04 09:56:20 +0100

    Endret env til å bruke filer

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -2,5 +2,9 @@ import environment from './environment'
 
 export const appUrl = (path: string): string => {
 	const strippedPath = path.startsWith('/') ? path.substring(1) : path
-	return `${environment.baseUrl}${strippedPath}`
+	return `${getBaseUrl()}${strippedPath}`
+}
+
+const getBaseUrl = (): string => {
+	return environment.baseUrl.replace(/\/pr-\d+\//, '/') // Fjerner "pr-DDD/" om det er en pr branch
 }
