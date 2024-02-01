5714e2a3e5bb9e354ffc0fc390b6117d5f57e419
MartheFH <136322563+MartheFH@users.noreply.github.com>
2024-02-01 16:20:25 +0100

    Legge til App-switcher og fjerne utlogging (#435)

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,30 +1,82 @@
-import { InternalHeader, Link, Panel } from '@navikt/ds-react'
-import React, { useState } from 'react'
+import { ExternalLinkIcon, MenuGridIcon } from '@navikt/aksel-icons'
+import { Dropdown, InternalHeader, Spacer } from '@navikt/ds-react'
+import React, { useRef } from 'react'
+import { Link } from 'react-router-dom'
 
 import { FORSIDE_PAGE_ROUTE } from '../../navigation'
 import { useDataStore } from '../../store/data-store'
-import { appUrl } from '../../utils/url-utils'
+import { mulighetsrommetAdminUrl, mulighetsrommetSanityUrl, mulighetsrommetVeilederUrl } from '../../utils/url-utils'
 import styles from './Header.module.scss'
 
 export const Header = (): React.ReactElement => {
 	const { innloggetAnsatt } = useDataStore()
-	const [ showDropdown, setShowDropdown ] = useState(false)
+
+	const tiltakstyperLinkRef = useRef<HTMLAnchorElement>(null)
+	const avtalerLinkRef = useRef<HTMLAnchorElement>(null)
+	const gjennomforingerLinkRef = useRef<HTMLAnchorElement>(null)
+	const individuelleGjennomforingerLinkRef = useRef<HTMLAnchorElement>(null)
+	const veilederflateLinkRef = useRef<HTMLAnchorElement>(null)
+	const notifikasjonerLinkRef = useRef<HTMLAnchorElement>(null)
+	const endringsmeldingerLinkRef = useRef<HTMLAnchorElement>(null)
 
 	return (
 		<InternalHeader className={styles.header} data-testid="innlogget-header">
 			<InternalHeader.Title href={FORSIDE_PAGE_ROUTE} aria-label="lenke til startsiden">
 				NAV Tiltaksadministrasjon
 			</InternalHeader.Title>
-			<InternalHeader.UserButton
-				name={innloggetAnsatt.navn}
-				className={styles.userBtn}
-				onClick={() => setShowDropdown(show => !show)}
-			/>
-			{showDropdown && (
-				<Panel className={styles.userDropdown} border onBlur={() => setShowDropdown(false)}>
-					<Link className={styles.logOutLink} href={appUrl('/oauth2/logout')}>Logg ut</Link>
-				</Panel>
-			)}
+			<Spacer />
+			<Dropdown>
+				<InternalHeader.Button as={Dropdown.Toggle}>
+					<MenuGridIcon style={{ fontSize: '1.5rem' }} title="Systemer og oppslagsverk" />
+				</InternalHeader.Button>
+
+				<Dropdown.Menu>
+					<Dropdown.Menu.GroupedList>
+						<Dropdown.Menu.GroupedList.Item onClick={() => tiltakstyperLinkRef.current?.click()} as="span">
+							<Link ref={tiltakstyperLinkRef} to={`${mulighetsrommetAdminUrl}/tiltakstyper`} target="_blank">
+								Tiltakstyper
+							</Link>
+						</Dropdown.Menu.GroupedList.Item>
+
+						<Dropdown.Menu.GroupedList.Item onClick={() => avtalerLinkRef.current?.click()} as="span">
+							<Link ref={avtalerLinkRef} to={`${mulighetsrommetAdminUrl}/avtaler`} target="_blank">
+								Avtaler
+							</Link>
+						</Dropdown.Menu.GroupedList.Item>
+
+						<Dropdown.Menu.GroupedList.Item onClick={() => gjennomforingerLinkRef.current?.click()} as="span">
+							<Link ref={gjennomforingerLinkRef} to={`${mulighetsrommetAdminUrl}/tiltaksgjennomforinger`} target="_blank">
+								Tiltaksgjennomføringer
+							</Link>
+						</Dropdown.Menu.GroupedList.Item>
+
+						<Dropdown.Menu.GroupedList.Item onClick={() => notifikasjonerLinkRef.current?.click()} as="span">
+							<Link ref={notifikasjonerLinkRef} to={`${mulighetsrommetAdminUrl}/notifikasjoner`} target="_blank">
+								Notifikasjoner
+							</Link>
+						</Dropdown.Menu.GroupedList.Item>
+
+						<Dropdown.Menu.GroupedList.Item onClick={() => individuelleGjennomforingerLinkRef.current?.click()} as="span">
+							<Link ref={individuelleGjennomforingerLinkRef} to={mulighetsrommetSanityUrl} target="_blank">
+								Individuelle tiltaksgjennomføringer <ExternalLinkIcon />
+							</Link>
+						</Dropdown.Menu.GroupedList.Item>
+
+						<Dropdown.Menu.GroupedList.Item onClick={() => veilederflateLinkRef.current?.click()} as="span">
+							<Link ref={veilederflateLinkRef} to={mulighetsrommetVeilederUrl} target="_blank">
+								Veilederflate forhåndsvisning <ExternalLinkIcon />
+							</Link>
+						</Dropdown.Menu.GroupedList.Item>
+
+						<Dropdown.Menu.GroupedList.Item onClick={() => endringsmeldingerLinkRef.current?.click()} as="span">
+							<Link ref={endringsmeldingerLinkRef} to="/">
+								Endringsmeldinger
+							</Link>
+						</Dropdown.Menu.GroupedList.Item>
+					</Dropdown.Menu.GroupedList>
+				</Dropdown.Menu>
+			</Dropdown>
+			<InternalHeader.User name={innloggetAnsatt.navn} description={innloggetAnsatt.navIdent ?? '...'} className={styles.userBtn} />
 		</InternalHeader>
 	)
 }
--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -1,4 +1,4 @@
-import environment from './environment'
+import environment, { erProdMiljo } from './environment'
 
 export const appUrl = (path: string): string => {
 	const strippedPath = path.startsWith('/') ? path.substring(1) : path
@@ -12,3 +12,6 @@ export const apiUrl = (path: string): string => {
 		.replace(/\/pr-\d+\//, '/')  // Fjerner "pr-DDD/" om det er en pr branch
 }
 
+export const mulighetsrommetAdminUrl = `https://mulighetsrommet-admin-flate.intern.${erProdMiljo ? '' : 'dev.'}nav.no`
+export const mulighetsrommetVeilederUrl = `https://mulighetsrommet-veileder-flate.intern.${erProdMiljo ? '' : 'dev.'}nav.no/preview`
+export const mulighetsrommetSanityUrl = 'https://mulighetsrommet-sanity-studio.intern.nav.no/prod/desk'
