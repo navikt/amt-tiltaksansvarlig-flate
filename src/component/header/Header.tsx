03322871b285159053b113209ec416fb95cacb61
Marthe Frogner Holm <marthe@Mac.localdomain>
2025-01-17 10:55:14 +0100

    Endre url for tiltaksgjennomforinger til gjennomforinger

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -20,55 +20,121 @@ export const Header = (): React.ReactElement => {
 	const endringsmeldingerLinkRef = useRef<HTMLAnchorElement>(null)
 
 	return (
-		<InternalHeader className={styles.header} data-testid="innlogget-header">
-			<InternalHeader.Title href={FORSIDE_PAGE_ROUTE} aria-label="lenke til startsiden">
+		<InternalHeader
+			className={styles.header}
+			data-testid="innlogget-header"
+		>
+			<InternalHeader.Title
+				href={FORSIDE_PAGE_ROUTE}
+				aria-label="lenke til startsiden"
+			>
 				NAV Tiltaksadministrasjon
 			</InternalHeader.Title>
 			<Spacer />
 			<Dropdown>
 				<InternalHeader.Button as={Dropdown.Toggle}>
-					<MenuGridIcon style={{ fontSize: '1.5rem' }} title="Systemer og oppslagsverk" />
+					<MenuGridIcon
+						style={{ fontSize: '1.5rem' }}
+						title="Systemer og oppslagsverk"
+					/>
 				</InternalHeader.Button>
 
 				<Dropdown.Menu>
 					<Dropdown.Menu.GroupedList>
-						<Dropdown.Menu.GroupedList.Item onClick={() => tiltakstyperLinkRef.current?.click()} as="span">
-							<Link ref={tiltakstyperLinkRef} to={`${tiltaksadministrasjonUrl}/tiltakstyper`} target="_blank">
+						<Dropdown.Menu.GroupedList.Item
+							onClick={() => tiltakstyperLinkRef.current?.click()}
+							as="span"
+						>
+							<Link
+								ref={tiltakstyperLinkRef}
+								to={`${tiltaksadministrasjonUrl}/tiltakstyper`}
+								target="_blank"
+							>
 								Tiltakstyper
 							</Link>
 						</Dropdown.Menu.GroupedList.Item>
 
-						<Dropdown.Menu.GroupedList.Item onClick={() => avtalerLinkRef.current?.click()} as="span">
-							<Link ref={avtalerLinkRef} to={`${tiltaksadministrasjonUrl}/avtaler`} target="_blank">
+						<Dropdown.Menu.GroupedList.Item
+							onClick={() => avtalerLinkRef.current?.click()}
+							as="span"
+						>
+							<Link
+								ref={avtalerLinkRef}
+								to={`${tiltaksadministrasjonUrl}/avtaler`}
+								target="_blank"
+							>
 								Avtaler
 							</Link>
 						</Dropdown.Menu.GroupedList.Item>
 
-						<Dropdown.Menu.GroupedList.Item onClick={() => gjennomforingerLinkRef.current?.click()} as="span">
-							<Link ref={gjennomforingerLinkRef} to={`${tiltaksadministrasjonUrl}/tiltaksgjennomforinger`} target="_blank">
+						<Dropdown.Menu.GroupedList.Item
+							onClick={() =>
+								gjennomforingerLinkRef.current?.click()
+							}
+							as="span"
+						>
+							<Link
+								ref={gjennomforingerLinkRef}
+								to={`${tiltaksadministrasjonUrl}/gjennomforinger`}
+								target="_blank"
+							>
 								Tiltaksgjennomføringer
 							</Link>
 						</Dropdown.Menu.GroupedList.Item>
 
-						<Dropdown.Menu.GroupedList.Item onClick={() => notifikasjonerLinkRef.current?.click()} as="span">
-							<Link ref={notifikasjonerLinkRef} to={`${tiltaksadministrasjonUrl}/notifikasjoner`} target="_blank">
+						<Dropdown.Menu.GroupedList.Item
+							onClick={() =>
+								notifikasjonerLinkRef.current?.click()
+							}
+							as="span"
+						>
+							<Link
+								ref={notifikasjonerLinkRef}
+								to={`${tiltaksadministrasjonUrl}/notifikasjoner`}
+								target="_blank"
+							>
 								Notifikasjoner
 							</Link>
 						</Dropdown.Menu.GroupedList.Item>
 
-						<Dropdown.Menu.GroupedList.Item onClick={() => individuelleGjennomforingerLinkRef.current?.click()} as="span">
-							<Link ref={individuelleGjennomforingerLinkRef} to={mulighetsrommetSanityUrl} target="_blank">
-								Individuelle tiltaksgjennomføringer <ExternalLinkIcon />
+						<Dropdown.Menu.GroupedList.Item
+							onClick={() =>
+								individuelleGjennomforingerLinkRef.current?.click()
+							}
+							as="span"
+						>
+							<Link
+								ref={individuelleGjennomforingerLinkRef}
+								to={mulighetsrommetSanityUrl}
+								target="_blank"
+							>
+								Individuelle tiltaksgjennomføringer{' '}
+								<ExternalLinkIcon />
 							</Link>
 						</Dropdown.Menu.GroupedList.Item>
 
-						<Dropdown.Menu.GroupedList.Item onClick={() => veilederflateLinkRef.current?.click()} as="span">
-							<Link ref={veilederflateLinkRef} to={navArbeidsmarkedstiltakUrl} target="_blank">
-								Veilederflate forhåndsvisning <ExternalLinkIcon />
+						<Dropdown.Menu.GroupedList.Item
+							onClick={() =>
+								veilederflateLinkRef.current?.click()
+							}
+							as="span"
+						>
+							<Link
+								ref={veilederflateLinkRef}
+								to={navArbeidsmarkedstiltakUrl}
+								target="_blank"
+							>
+								Veilederflate forhåndsvisning{' '}
+								<ExternalLinkIcon />
 							</Link>
 						</Dropdown.Menu.GroupedList.Item>
 
-						<Dropdown.Menu.GroupedList.Item onClick={() => endringsmeldingerLinkRef.current?.click()} as="span">
+						<Dropdown.Menu.GroupedList.Item
+							onClick={() =>
+								endringsmeldingerLinkRef.current?.click()
+							}
+							as="span"
+						>
 							<Link ref={endringsmeldingerLinkRef} to="/">
 								Endringsmeldinger
 							</Link>
@@ -76,7 +142,11 @@ export const Header = (): React.ReactElement => {
 					</Dropdown.Menu.GroupedList>
 				</Dropdown.Menu>
 			</Dropdown>
-			<InternalHeader.User name={innloggetAnsatt.navn} description={innloggetAnsatt.navIdent ?? '...'} className={styles.userBtn} />
+			<InternalHeader.User
+				name={innloggetAnsatt.navn}
+				description={innloggetAnsatt.navIdent ?? '...'}
+				className={styles.userBtn}
+			/>
 		</InternalHeader>
 	)
 }
