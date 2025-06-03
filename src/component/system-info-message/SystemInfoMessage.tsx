dc231cefbdce49900f7e6acfa4d1496ace7e2fa3
MartheFH <136322563+MartheFH@users.noreply.github.com>
2025-06-03 08:37:11 +0200

    Fiks infomelding etter lansering av kurs-tiltak (#806)

--- src/component/system-info-message/SystemInfoMessage.tsx
+++ src/component/system-info-message/SystemInfoMessage.tsx
@@ -1,49 +1,32 @@
 import { Alert, BodyShort, Heading, Link } from '@navikt/ds-react'
 import React from 'react'
 
-import useLocalStorage from '../../hooks/useLocalStorage'
 import styles from './SystemInfoMessage.module.scss'
 
 export const SystemInfoMessage = () => {
-	const [ visMelding, setVisMelding ] = useLocalStorage(
-		'amt-tiltaksansvarlig-vis-systemmelding',
-		true
-	)
-
-	return visMelding ? (
-		<Alert
-			variant="info"
-			size="small"
-			closeButton
-			onClose={() => setVisMelding(false)}
-		>
+	return (
+		<Alert variant="info" size="small">
 			<Heading size="small" level="1" spacing>
-				Fra 3. juni kan du ikke endre deltakelser i Arena for tiltakene gruppe-AMO, gruppe fag- og
-				yrkesopplæring og jobbklubb
+				Fra 3. juni kan du ikke endre deltakelser i Arena for tiltakene
+				gruppe-AMO, gruppe fag- og yrkesopplæring og jobbsøkerkurs
 			</Heading>
 			<BodyShort size="small">
-				Den nye løsningen for påmelding og endring av deltakelse for Nav-veileder blir snart tilgjengelig for
-				disse tiltakene. Tiltaksarrangør kan sende forslag om endringer direkte til Nav-veileder.
+				Nav-veileder har fått ny løsning i Modia for påmelding og
+				endring av deltakelse. Tiltaksarrangør kan sende forslag om
+				endringer direkte til Nav-veileder.
 			</BodyShort>
-			<br/>
+			<br />
 			<BodyShort size="small">
-				Nye vurderinger fra tiltaksarrangør vises allerede nå i deltakerlisten til hver gjennomføring. Eldre
-				vurderinger vil bli liggende i denne tjenesten i en periode etter overgang til ny løsning.{' '}
-			</BodyShort>
-			<Link className={styles.alert_text}
-				href="https://navno.sharepoint.com/sites/fag-og-ytelser-arbeid-tiltak-og-virkemidler/SitePages/Administrering-av-deltakelse-p%C3%A5-kurstiltak.aspx">
-				Les mer om ny løsning på Navet.
-			</Link>
-			<BodyShort size="small">
-				<b>FRIST: Alle endringsmeldinger for disse tiltakene må legges inn i Arena innen 2. juni kl. 15</b>
-				<br/>
-				Fra 27. mai kl. 15 kan ikke tiltaksarrangør sende inn nye endringsmeldinger.
+				Nye vurderinger fra tiltaksarrangør vises i deltakerlisten til
+				hver gjennomføring. Eldre vurderinger vil bli liggende i denne
+				tjenesten i en periode etter overgang til ny løsning.{' '}
 			</BodyShort>
 			<Link
 				className={styles.alert_text}
-				href="https://www.nav.no/samarbeidspartner/nytt-i-deltakeroversikten">
-				Tiltaksarrangør er informert her på nav.no.
+				href="https://navno.sharepoint.com/sites/fag-og-ytelser-arbeid-tiltak-og-virkemidler/SitePages/Administrering-av-deltakelse-p%C3%A5-kurstiltak.aspx"
+			>
+				Les mer om ny løsning på Navet.
 			</Link>
 		</Alert>
-	) : null
+	)
 }
